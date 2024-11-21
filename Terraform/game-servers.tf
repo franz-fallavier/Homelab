6b672095d97598ec4fe972f83f73b696fa227628
franz fallavier <franzfallavier@gmail.com>
2024-11-21 23:25:14 +0100

    Terraform - Merge some .tf files

--- Terraform/game-servers.tf
+++ Terraform/game-servers.tf
@@ -1,16 +1,16 @@
-resource "proxmox_vm_qemu" "gitlab" {
-  name        = "gitlab"
-  desc        = "Ubuntu Cloud Hosting GitLab"
-  vmid        = "210"
+resource "proxmox_vm_qemu" "minecraft-server" {
+  name        = "minecraft-server"
+  desc        = "Ubuntu Cloud Hosting Minecraft Servers"
+  vmid        = "410"
   target_node = "zigris"
   agent       = 1
-  clone       = "ubuntu-cloud-24.04-0080"
-  cores       = 8
+  clone       = "ubuntu-cloud-24.04-0040"
+  cores       = 4
   sockets     = 1
   numa        = true
   vcpus       = 0
   cpu         = "host"
-  memory      = 8192
+  memory      = 4096
   onboot      = true
   startup     = "order=1"
 
@@ -42,36 +42,35 @@ resource "proxmox_vm_qemu" "gitlab" {
       scsi0 {
         disk {
           storage = "local-nvme"
-          size    = 80
+          size    = 40
         }
       }
     }
   }
   boot         = "order=scsi0"
   os_type      = "cloud-init"
-  searchdomain = "gitlab"
+  searchdomain = "minecraft-server"
   nameserver   = "10.0.2.3"
   ipconfig0    = "ip=dhcp"
   ciuser       = var.proxmox_cloudinit_user
   sshkeys      = var.proxmox_cloudinit_public_key
 }
 
-resource "proxmox_vm_qemu" "gitlab-runner" {
-  count       = 2
-  name        = "gitlab-runner-${count.index + 1}"
-  desc        = "Ubuntu Cloud Hosting GitLab-Runner"
-  vmid        = "21${count.index + 1}"
+resource "proxmox_vm_qemu" "factorio-server" {
+  name        = "factorio-server"
+  desc        = "Ubuntu Cloud Hosting Factorio Servers"
+  vmid        = "420"
   target_node = "zigris"
   agent       = 1
-  clone       = "ubuntu-cloud-24.04-0020"
-  cores       = 2
+  clone       = "ubuntu-cloud-24.04-0040"
+  cores       = 4
   sockets     = 1
   numa        = true
   vcpus       = 0
   cpu         = "host"
-  memory      = 2048
+  memory      = 4096
   onboot      = true
-  startup     = "order=2,up=15"
+  startup     = "order=1"
 
   network {
     bridge = "vmbr0"
@@ -101,14 +100,14 @@ resource "proxmox_vm_qemu" "gitlab-runner" {
       scsi0 {
         disk {
           storage = "local-nvme"
-          size    = 20
+          size    = 40
         }
       }
     }
   }
   boot         = "order=scsi0"
   os_type      = "cloud-init"
-  searchdomain = "gitlab-runner-${count.index + 1}"
+  searchdomain = "factorio-server"
   nameserver   = "10.0.2.3"
   ipconfig0    = "ip=dhcp"
   ciuser       = var.proxmox_cloudinit_user
